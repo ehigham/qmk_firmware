5c890076755f8fe5a76d7de8e07c11e9b10a73a1
Edmund Higham <edhigham@gmail.com>
2024-04-02 16:46:21 -0400

    swap Up and Down

--- keyboards/gizmo_engineering/gk6/keymaps/ehigham/keymap.c
+++ keyboards/gizmo_engineering/gk6/keymaps/ehigham/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | CTRL |  Gui |  Alt |   Fn | Lower|    Space    |Raise | Left |   Up | Down |Right |
+ * | CTRL |  Gui |  Alt |   Fn | Lower|    Space    |Raise | Left | Down |   Up |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_1x2u(
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC,    KC_Q,    KC_W, KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,     KC_I,    KC_O,    KC_P, KC_BSPC,
    KC_TAB,    KC_A,    KC_S, KC_D,  KC_F,  KC_G,  KC_H,  KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT,
   KC_LSFT,    KC_Z,    KC_X, KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-  KC_LCTL, KC_LGUI, KC_LALT,   FN, LOWER,     KC_SPC,   RAISE,  KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT
+  KC_LCTL, KC_LGUI, KC_LALT,   FN, LOWER,     KC_SPC,   RAISE,  KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
 ),
 
 /* Raise
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  <<  |  >>  | Mute | Vol- | Vol+ |      |   1  |   2  |   3  |   =  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |   0  |   .  | PgUp | PgDn | NumLk|
+ * |      |      |      |      |      |             |   0  |   .  | PgDn | PgUp | NumLk|
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_1x2u(
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_PSCR, _______, _______, _______, _______, _______,   KC_P7,   KC_P8,   KC_P9, KC_PMNS,  KC_DEL,
   KC_CAPS, KC_HOME, KC_END,  KC_INS,  KC_MPLY, _______, _______,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, _______,
   _______, KC_MPRV, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, _______,
-  _______, _______, _______, _______, _______,     _______,        KC_P0, KC_PDOT, KC_PGUP, KC_PGDN,  KC_NUM
+  _______, _______, _______, _______, _______,     _______,        KC_P0, KC_PDOT, KC_PGDN, KC_PGUP,  KC_NUM
 ),
 
 /* FN
